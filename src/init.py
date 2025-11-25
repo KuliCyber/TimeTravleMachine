43bc2dbb8641f2f0eee4306cf016bc4e38366217
KuliCyber <muhammadnadzif960@gmail.com>
2025-11-25 13:35:25 +0700

    init.py

---
+++ src/init.py
@@ -0,0 +1,7 @@
+from .time_machine import VirtualTimeMachine
+from .simple_time_machine import SimpleTimeMachine
+from .quantum_core import QuantumEnergyCore
+from .paradox_solver import ParadoxSolver
+
+__version__ = "1.0.0"
+__all__ = ['VirtualTimeMachine', 'SimpleTimeMachine', 'QuantumEnergyCore', 'ParadoxSolver']
